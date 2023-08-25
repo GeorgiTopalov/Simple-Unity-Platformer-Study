2b5ed222592380c1f6d7b8bc1648c8c64666fa23
Georgi Topalov <g.s.topalov@gmail.com>
2023-08-25 14:39:52 +0700

    Added death on collision with traps with animation.

---
+++ Generic Platformer Game/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("death");
+    }
+}
