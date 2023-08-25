f74c4b3687f8aa9b472f40203aca306e15fe9563
Georgi Topalov <g.s.topalov@gmail.com>
2023-08-25 16:11:04 +0700

    Added second box collider to moving platform for not sticking on the sides

--- Generic Platformer Game/Assets/Scripts/StickyPlatform.cs
+++ Generic Platformer Game/Assets/Scripts/StickyPlatform.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class StickyPlatform : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Character")
         {
             collision.gameObject.transform.SetParent(transform);
         }
     }
-
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Character")
         {
