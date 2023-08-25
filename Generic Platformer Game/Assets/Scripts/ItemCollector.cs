e72e8fd101d211c037a3f1813accaaf8d53dd208
Georgi Topalov <g.s.topalov@gmail.com>
2023-08-25 14:07:07 +0700

    Added Text for points with scripted counter.

--- Generic Platformer Game/Assets/Scripts/ItemCollector.cs
+++ Generic Platformer Game/Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int collectibles = 0;
+
+    [SerializeField] private TextMeshProUGUI collectiblesText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectable"))
         {
             Destroy(collision.gameObject);
+            collectibles++;
+            collectiblesText.text = "Points: " + collectibles;
         }
     }
 }
